4be25ba246b12edce02eb0a64b0e8341b11fb59b
davidovich-dn <davidovich.dn@hotmail.com>
2019-03-24 16:05:50 +0300

    about 50% done

---
+++ src/test/java/data/Data.java
@@ -0,0 +1,35 @@
+package data;
+
+public class Data {
+
+    private final String URL = "https://gmail.com/";
+    private final String LOGIN = "davidovich.dn.tests";
+    private final String EMAIL = "davidovich.dn.tests@gmail.com";
+    private final String PASSWORD = "n4nPLCzqit3wCv5";
+    private final String SUBJECT = "test letter";
+    private final String MAIL_BODY = "some text for mail body";
+
+    public String getURL(){
+        return URL;
+    }
+
+    public String getLOGIN(){
+        return LOGIN;
+    }
+
+    public String getEMAIL(){
+        return EMAIL;
+    }
+
+    public String getPASSWORD(){
+        return PASSWORD;
+    }
+
+    public String getSUBJECT(){
+        return SUBJECT;
+    }
+
+    public String getMAIL_BODY(){
+        return MAIL_BODY;
+    }
+}
